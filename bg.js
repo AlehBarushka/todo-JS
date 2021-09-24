138bb74c705761186409ffc78b1f8b7fb58c729b
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2021-09-24 10:40:50 +0300

    finished todo and add bg images

---
+++ bg.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body');
+const IMAGE_NUMBER = 4;
+
+function showImage(number) {
+  const img = new Image();
+  img.src = `img/${number + 1}.jpg`;
+  img.classList.add('bgImage');
+  body.prepend(img);
+}
+
+function getRandom() {
+  const number = Math.floor(Math.random() * IMAGE_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = getRandom();
+  showImage(randomNumber);
+}
+init();
