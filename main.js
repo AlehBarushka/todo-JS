96ab3a6b50fa915200555f6559f37ab8ad4983ad
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2021-09-27 11:09:56 +0300

    add ES6 modules

---
+++ main.js
@@ -0,0 +1,14 @@
+import { initBg } from './modules/bg.js';
+import { initTime } from './modules/clock.js';
+import { initGreeting } from './modules/greeting.js';
+import { initToDoApp } from './modules/todo-app.js';
+import { initWeather } from './modules/weather.js';
+
+function main() {
+  initTime();
+  initGreeting();
+  initBg();
+  initWeather();
+  initToDoApp();
+}
+export { main };
---
+++ modules/weather.js
@@ -0,0 +1,58 @@
+const COORDS_LS = 'coords';
+const API_KEY = '75787c308f283527b34dff6f9ed6f9b8';
+
+const wetherConteiner = document.querySelector('.js-weather');
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      const temperature = json.main.temp;
+      const place = json.name;
+      wetherConteiner.innerText = `${temperature} @${place}`;
+    });
+}
+
+function saveCoords(positionObj) {
+  localStorage.setItem(COORDS_LS, JSON.stringify(positionObj));
+}
+
+function geoSuccessHundle(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const positionObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(positionObj);
+  getWeather(latitude, longitude);
+}
+
+function geoErroHundle() {
+  const loadedCoords = JSON.parse(coords);
+  console.log(loadedCoords);
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(geoSuccessHundle, geoErroHundle);
+}
+
+function getCoords() {
+  const coords = localStorage.getItem(COORDS_LS);
+  if (coords === null) {
+    askForCoords();
+  } else {
+    const loadedCoords = JSON.parse(coords);
+    getWeather(loadedCoords.latitude, loadedCoords.longitude);
+  }
+}
+
+function initWeather() {
+  getCoords();
+}
+
+export { initWeather };
